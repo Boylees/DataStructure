e444822f42f9465f60cc6ee584ba134572a33a0c
Boylees <shenmiheiyirenx@gmail.com>
2024-09-29 09:46:54 +0800

    2.3.1

--- List/2.3线性表的链式表示和实现/2.3线性表的静态单链表存储结构.c
+++ List/2.3线性表的链式表示和实现/2.3线性表的静态单链表存储结构.c
@@ -4,3 +4,11 @@ typedef struct {
     ElemType data;
     int cur;
 }component, SLinkList[MAXSIZE];
+
+int LocateElem_SL(SLinkList S, ElemType e) {
+    int i = S[0].cur;
+    while (i && S[i].data != e) {
+        i = S[i].cur;
+    }
+    return i;
+}
